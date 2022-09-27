ba8a4cd7e588a9f5e004a4b78524c60fe293bd1d
Arthur Alencar <ioutlet@air-de-ioutlet.lan>
2022-09-27 12:47:49 +0100

    check bottom logic completet

---
+++ app.js
@@ -0,0 +1,50 @@
+"use strict";
+
+let count = 20;
+const guess = document.querySelector(".guess");
+const check = document.querySelector(".check");
+const message = document.querySelector(".message");
+const number = document.querySelector(".number");
+const again = document.querySelector(".again");
+const score = document.querySelector(".score");
+
+
+let randomNumber = function () {
+  return Math.round(Math.random() * 20);
+};
+
+let newRandom = randomNumber();
+console.log(newRandom);
+check.addEventListener("click", function () {
+        game();
+  
+});
+
+again.addEventListener("click", function() {
+    game();
+    number.textContent = "?";
+    
+
+})
+
+
+function game() {
+
+    let newGuess = Number(guess.value);
+
+    if(!newGuess) {
+      message.textContent = "⛔️ No number"
+    }else if(newGuess == newRandom) {
+      number.textContent = newRandom;
+      message.textContent = "🎉 Number correct";
+    } else if(newGuess > newRandom) {
+      message.textContent = "To high";
+      count--;
+    } else if(newGuess < newRandom) {
+      message.textContent = "To low";
+      count--;
+    }
+    score.textContent = count;
+    
+
+}
